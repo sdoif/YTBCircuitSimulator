dc8b27ebc9e150b93eb8bff3239e8426673eebb2
sts219 <sts219@ic.ac.uk>
2020-05-25 17:31:45 +0100

    Outlined initial thoughts regarding input management, created NetlistReader header

---
+++ ProjectFiles/netlist_reader.hpp
@@ -0,0 +1,23 @@
+#ifndef NetlistReader_hpp
+#define NetlistReader_hpp
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class NetlistReader
+{
+private:
+  istream &src;
+
+public:
+  //Creattes a new netlist reader using the given input stream
+  NetlistReader(istream &src);
+
+  /*Reads a line of from the associated stream.
+  Once '.end' is reached returns an empty vector and should not be called again
+  */
+  vector<string> next();
+}
