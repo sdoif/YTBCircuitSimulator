2eaf4ddaec93a797b822cca46ea9951bae443693
sts219 <sts219@ic.ac.uk>
2020-05-30 16:08:27 +0100

    Finalised initial input management for simulator

--- ProjectFiles/simulator.cpp
+++ ProjectFiles/simulator.cpp
@@ -5,7 +5,10 @@ using namespace Eigen;
 
 int main()
 {
+  //Netlist management
   vector<vector<string>> input;
+  vector<string> tran;
+  string node_max;
   NetlistReader reader(cin);
   while(1)
   {
@@ -13,7 +16,41 @@ int main()
     if(line.empty()){
       break;
     }
-    input.push_back(line);
+    if(line[0]==".tran"){
+      tran = line;
+    }
+    else{
+      if(line[1]>node_max){
+        node_max = line[1];
+      }
+      if(line[2]>node_max){
+        node_max = line[2];
+      }
+      input.push_back(line);
+    }
   }
-  //For calculating number of nodes, stoi can be used on '000x' and will ignore 0s
+  if(tran.empty()){
+    cerr<<"No simulation settings specified"<<endl;
+    return 1;
+  }
+
+  //Creating appropriate matrices / vectors
+  int nodes;
+  if(node_max.find('N')!=string::npos){
+    node_max.erase('N');
+  }
+  nodes = stoi(node_max)+1;
+  Matrix<double, Dynamic, Dynamic, 0, 16, 16> con_s;
+  if(nodes<17){
+    con_s.resize(nodes, nodes);
+  }
+  MatrixXd con_l;
+  if(nodes>16){
+    con_l.resize(nodes,nodes);
+  }
+  VectorXd v(nodes);
+  VectorXd i(nodes);
+
+  //TO-DO: Update the values of the conductance matrix / current / voltage vectors for each component
+
 }
--- ProjectFiles/test.cpp
+++ ProjectFiles/test.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <Eigen/Dense>
 
 using namespace std;
+using namespace Eigen;
 
 int main()
 {
@@ -9,4 +11,11 @@ int main()
   cin>>in;
   int x = stoi(in);
   cout<<x<<endl;
+  MatrixXd con_l;
+  cout<<"test: "<<con_l<<endl;
+  if(x<17){
+    con_l.resize(x,x);
+  }
+  con_l << 1,2,3,4,5,6,7,8,9;
+  cout<<con_l<<endl;
 }
