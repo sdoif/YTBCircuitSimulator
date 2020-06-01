91a38f373805e96a882511cc671b2ba0635082e5
sts219 <sts219@ic.ac.uk>
2020-06-01 18:46:09 +0100

    Testing done on using ternary operator, unnsuccessful for initialising different types of matrices conditionally.

--- ProjectFiles/test.cpp
+++ ProjectFiles/test.cpp
@@ -12,9 +12,8 @@ int main()
   int x = stoi(in);
   cout<<x<<endl;
   bool small = x<17;
-  small ? Matrix<double, Dynamic, Dynamic, 0, 16, 16> con; : MatrixXd con;
-  Matrix<double, Dynamic, Dynamic, 0, 16, 16> con_s;
   MatrixXd con_l;
+  Matrix<double, Dynamic, Dynamic, 0, 16, 16> (small ? con : con_s);
 
   con_s.resize(3,3);
   con_s << 1,2,3, 4,5,6, 7,8,9;
