293298106340a805f65450afb57dbe1e143e8a93
sts219 <sts219@ic.ac.uk>
2020-06-11 21:36:25 +0100

    Continued filter testing

--- Testing/filter_test.sh
+++ Testing/filter_test.sh
@@ -4,9 +4,9 @@ set -e
 
 echo "Testing..."
 
-<AC_CapacitorNetlist ./simulator >F_AC_C.txt
-<DC_CapacitorNetlist ./simulator >F_DC_C.txt
-<AC_InductorNetlist ./simulator >F_AC_L.txt
-<DC_InductorNetlist ./simulator >F_DC_L.txt
+<AC_CapacitorNetlist.txt ./simulator >F_AC_C.txt
+<DC_CapacitorNetlist.txt ./simulator >F_DC_C.txt
+<AC_InductorNetlist.txt ./simulator >F_AC_L.txt
+<DC_InductorNetlist.txt ./simulator >F_DC_L.txt
 
 echo "Output Captured"
