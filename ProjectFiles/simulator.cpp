7fe303b1cde143366b1798925b2ce18e521462ca
rs1018 <rs1018@imperial.ac.uk>
2020-06-14 00:53:17 +0100

    Salman is a fucking genius

--- ProjectFiles/simulator.cpp
+++ ProjectFiles/simulator.cpp
@@ -152,7 +152,7 @@ int main()
             if(con_s(stoi(line[2])-1)!=0){
               con_s(stoi(line[1])-1, x) = con_s(stoi(line[2])-1, x);
               con_s(stoi(line[2])-1, x) = 0;
-              con_s(stoi(line[1])-1, stoi(line[1])-1) += abs(con_s(stoi(line[1])-1, x));
+              con_s(stoi(line[1])-1, stoi(line[2])-1) += abs(con_s(stoi(line[1])-1, x));
             }
           }
 
@@ -189,28 +189,14 @@ int main()
       //All other cases when it is connected to 2 non-reference nodes
       else{
         //Check for second time voltage source appears
-        double sc = 0;
         if(stoi(line[1]) > stoi(line[2])){
           //Copying values from first row into second row and overwrite first row
           for(int x=0; x<con_s.cols(); x++){
-            if(con_s(stoi(line[2])-1)!=0){
             con_s(stoi(line[1])-1, x) += con_s(stoi(line[2])-1, x);
             con_s(stoi(line[2])-1, x) = 0;
-            con_s(stoi(line[1])-1, stoi(line[1])-1) += abs(con_s(stoi(line[1])-1, x));
-            }
           }
-          //find total conductance connected positive end of supernode
-          for(int y=0; y<con_s.cols(); y++){
-            if(y < stoi(line[1])-1){
-              sc += con_s(stoi(line[1])-1, y);
-            }
-          }
-          //Current vector value equal to value of source multiplied by conductance at positive terminal
-          i_s(stoi(line[1])-1) = ctod(line[3])*sc;
           //Move current vector value from first row into second row if current source present
           i_s(stoi(line[1])-1) += i_s(stoi(line[2])-1);
-          //Making of supernode means 0 conductance between nodes
-          con_s(stoi(line[1])-1, stoi(line[2])-1) = 0;
           //Add in 1 and -1 to first row to represent voltage source
           con_s(stoi(line[2])-1, stoi(line[2])-1) = 1;
           con_s(stoi(line[2])-1, stoi(line[1])-1) = -1;
